4d94eba8b9c4a995ea2cfa1eb6410aedc8adbc00
Meghan Letizia <44990337+megletizia@users.noreply.github.com>
2019-04-03 21:56:57 -0400

    completed coding challenge for creating arrays and adding/removing specific values

---
+++ store-data-in-arrays/musketeers.js
@@ -0,0 +1,11 @@
+/*
+Do the following:
+
+Create an array named musketeers containing values - “Athos”, “Porthos” and “Aramis”
+Add the “D’Artagnan” value to the array
+Remove poor Aramis
+*/
+
+var musketeers = ['Athos','Porthos', 'Aramis'];
+musketeers.push("D'Artagnan");
+musketeers.splice(2,1);
